ce685aaee7506bf5f18a7807cbeeb69a20edb630
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-05-07 17:10:14 +0530

    build created

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.static("dist"))
 app.use(cookieParser());
 app.use(morgan("dev"));
 
